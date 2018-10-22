5a856e8d1a638d21b91ca1b29d724ce347599636
Eliethesaiyan <eliemagambo@gmail.com>
2018-10-22 13:37:56 +0900

    Added Gradient descent in numpy and tensorflow

--- operations/linear-regression.py
+++ operations/linear-regression.py
@@ -3,6 +3,7 @@
 '''
 
 import tensorflow as tf
+import numpy as np
 
 tf.enable_eager_execution()
 
@@ -16,6 +17,36 @@ y = (y_raw - y_raw.min()) / (y_raw.max() - y_raw.min())
 
 
 #gradient descendent 
+a, b = 0, 0
+
+num_epoch = 10000
+learning_rate = 1e-3
+
+# calculate gradients by hand in numpy
+for e in range(num_epoch):
+    y_pred = a * X + b
+    grad_a, grad_b = (y_pred - y).dot(X), (y_pred - y).sum()
+    a, b = a - learning_rate * grad_a, b - learning_rate * grad_b
+    print("a = "+str(a),"b = "+str(b))
+
+
+# using  the Gradient Descedent Optimizer from tensorflow
+X = tf.constant(X)
+y = tf.constant(y)
+
+c  = tf.get_variable('c', dtype=tf.float32, shape =[], initializer = tf.zeros_initializer)
+d  = tf.get_variable('d', dtype=tf.float32, shape =[], initializer = tf.zeros_initializer)
+variables =[c,d]
+optimizer = tf.train.GradientDescentOptimizer(learning_rate= learning_rate)
+
+for e in range(num_epoch):
+    # Use tape to record gradient info of the loss
+    with tf.GradientTape() as tape:
+        y_pred = c * X + d 
+        loss = 0.5 * tf.reduce_sum(tf.square(y_pred - y))
+    grads = tape.gradient(loss,variables)
+    print(grads[0].numpy(), grads[1].numpy())
+    optimizer.apply_gradients(grads_and_vars=zip(grads, variables))
 
 
 
