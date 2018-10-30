22e1e45a993bd4466a1e171aacfa9bdcf0d1eedc
eliethesaiyan <eliemagambo@gmail.com>
2018-10-30 20:37:58 +0900

    Corrected the network one hot encoding

--- model_and_layers/reinforcement-learning.py
+++ model_and_layers/reinforcement-learning.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 import random
 from collections import deque
 
+config = tf.ConfigProto()
 tf.enable_eager_execution()
 num_episodes = 500
 num_exploration_episodes = 100
@@ -22,12 +23,13 @@ class QNetwork(tf.keras.Model):
         super().__init__()
         self.dense1 = tf.keras.layers.Dense(units=24, activation=tf.nn.relu)
         self.dense2 = tf.keras.layers.Dense(units=24, activation=tf.nn.relu)
-        self.dense2 = tf.keras.layers.Dense(units=2)
+        self.dense3 = tf.keras.layers.Dense(units=2)
 
     def call(self, inputs):
         x = self.dense1(inputs)
         x = self.dense2(x)
         x = self.dense3(x)
+        return x
 
     def predict(self,inputs):
         q_values = self(inputs)
@@ -53,7 +55,7 @@ for episode_id in range(num_episodes):
             action = model.predict(
                      tf.constant(np.expand_dims(state, axis=0), dtype=tf.float32)).numpy()
             action = action[0]
-        nex_state, reward, done, info = env.step(action)
+        next_state, reward, done, info = env.step(action)
         reward = -10 if done else reward
         replay_buffer.append((state, action, reward, next_state, done))
         state = next_state
@@ -67,13 +69,15 @@ for episode_id in range(num_episodes):
             batch_state, batch_action, batch_reward, batch_next_state, batch_done = \
              [np.array(a, dtype=np.float32) for a in zip(*random.sample(replay_buffer,batch_size))]
 
-            q_value = model(tf.constant(batch_next_sate, dtype=tf.float32))
+            q_value = model(tf.constant(batch_next_state, dtype=tf.float32))
             y = batch_reward + (gamma * tf.reduce_max(q_value, axis=1)) * (1 - batch_done)
 
             
-            with tf,GradientTape() as tape:
-                loss = tf.losses.mean_squared_error(labels=y, prediction= tf.reduce_sum(model(tf.constant(batch_size)) *
-                     tf.one_hot(batch_action, depth=2), axis =1))
+            with tf.GradientTape() as tape, tf.device("/cpu:0"):
+                loss = tf.losses.mean_squared_error(
+                        labels=y, 
+                        predictions= tf.reduce_sum(model(tf.constant(batch_state)) *
+                                                   tf.one_hot((batch_action).astype(int), depth=2), axis = 1))
 
             grads = tape.gradient(loss,model.variables)
-            optimizer.apply_gradient(grads_and_vars=zip(grads,model.variables))
+            optimizer.apply_gradients(grads_and_vars=zip(grads,model.variables))
